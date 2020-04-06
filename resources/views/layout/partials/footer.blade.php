79ebb490e0dc8db9c6412bc5dd6e8b041deff694
babak <ashrafiseyedbabak@gmail.com>
2020-04-06 13:06:06 +0430

    Fix minor bug

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,10 +1,3 @@
-{{--<footer class="container py-5">--}}
-{{--    <div class="row">--}}
-{{--        <div class="col-12 col-md">--}}
-{{--            <small class="d-block mb-3 text-muted">&copy; 2020  </small>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</footer>--}}
 <footer class="page-footer font-small bg-dark text-light">
     <div class="container">
         <div class="row">
@@ -17,10 +10,10 @@
             </div>
             <div class="col m-4">
                 <h3>Contacts</h3>
-                <h5>Address : No. 5, Sarv Street, Valiasr Ave., Tehran, IRAN</h5>
-                <h5>Postal Code : 1968956193</h5>
-                <h5>Tel : +98 21 88662300</h5>
-                <h5>E-Mail : Info@paraye.com</h5>
+                <h5>Address : Something</h5>
+                <h5>Postal Code : ********</h5>
+                <h5>Tel :**-******</h5>
+                <h5>E-Mail : Info@info.com</h5>
             </div>
         </div>
     </div>
