c93b7514c7bb78fd983ebaaace943d4dcb5b624d
Rasoul <h.rostami.r@gmail.com>
2020-04-16 00:16:33 +0430

    Send email on order submit

--- app/Http/Controllers/Site/OrderController.php
+++ app/Http/Controllers/Site/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreOrder;
+use App\Mail\OrderSubmit;
 use App\Models\Address;
 use App\Models\Order;
 use App\Repositories\Contracts\OrderRepositoryInterface;
@@ -11,6 +12,7 @@ use App\Repositories\Contracts\ShoppingCartRepositoryInterface;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\View\View;
 
 class OrderController extends Controller implements OrderControllerInterface
@@ -72,6 +74,7 @@ class OrderController extends Controller implements OrderControllerInterface
         $address->save();
         $order = $this->orderRepository->checkout($address);
         flash('Order has been successfully submitted.');
+        Mail::to(Auth::user()->email)->send(new OrderSubmit());
         return redirect()->route('order.show', ['order' => $order->id]);
     }
 }
---
+++ resources/views/emails/orderSubmit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Email</title>
+</head>
+
+<body>
+<h2>Thank you, We will ship your order as soon as possible. Come back again!</h2>
+
+</body>
+
+</html>
